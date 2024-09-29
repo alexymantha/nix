1e7b2e312e356e51a32a0cb2da6e0b5bf4bcbc07
Alexy Mantha <alexy@mantha.dev>
2024-09-28 22:10:53 -0400

    allow devenv cachix
    Signed-off-by: Alexy Mantha <alexy@mantha.dev>

--- hosts/darwin/default.nix
+++ hosts/darwin/default.nix
@@ -5,7 +5,16 @@
 
   nix = {
     package = pkgs.nix;
-    settings.experimental-features = ["nix-command" "flakes"];
+    settings = {
+      experimental-features = "nix-command flakes";
+
+      substituters = [
+        "https://devenv.cachix.org"
+      ];
+      trusted-public-keys = [
+        "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+      ];
+    };
   };
 
   programs.zsh.enable = true;
