724b57de5e22f08d61341d2a03bf25a065d6da17
Alexy Mantha <alexy@mantha.dev>
2024-10-30 22:53:26 -0400

    tweak cmp
    Signed-off-by: Alexy Mantha <alexy@mantha.dev>

--- home-manager/configs/nvim/lua/plugins/cmp.lua
+++ home-manager/configs/nvim/lua/plugins/cmp.lua
@@ -4,18 +4,26 @@ return {
         'hrsh7th/nvim-cmp',
         event = "InsertEnter",
         dependencies = {
-            'hrsh7th/cmp-nvim-lsp',
             'hrsh7th/cmp-buffer',
-            'hrsh7th/cmp-path',
             'hrsh7th/cmp-cmdline',
             'hrsh7th/cmp-nvim-lsp',
-            'petertriho/cmp-git',
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-path',
             'onsails/lspkind.nvim',
+            'petertriho/cmp-git',
             'saadparwaiz1/cmp_luasnip',
         },
         config = function()
             local lspkind = require("lspkind")
             local cmp = require("cmp")
+            local luasnip = require("luasnip")
+
+            local has_words_before = function()
+                unpack = unpack or table.unpack
+                local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+                return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+            end
+
             cmp.setup({
                 formatting = {
                     format = lspkind.cmp_format({
@@ -36,26 +44,62 @@ return {
                         require('luasnip').lsp_expand(args.body)
                     end,
                 },
+                preselect = cmp.PreselectMode.None,
                 mapping = cmp.mapping.preset.insert({
                     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
                     ['<C-f>'] = cmp.mapping.scroll_docs(4),
                     ['<C-Space>'] = cmp.mapping.complete(),
-                    ['<Tab>'] = cmp.mapping.select_next_item(),
-                    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
-                    ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                    ["<Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                        elseif luasnip.locally_jumpable(1) then
+                            luasnip.jump(1)
+                        elseif has_words_before() then
+                            cmp.complete()
+                        else
+                            fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+                        end
+                    end, { "i", "s" }),
+                    ["<S-Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_prev_item()
+                        elseif luasnip.locally_jumpable(-1) then
+                            luasnip.jump(-1)
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+                    ['<CR>'] = cmp.mapping(function (fallback)
+                        if cmp.visible() then
+                            if luasnip.expandable() then
+                                luasnip.expand()
+                            else
+                                cmp.confirm({
+                                    select = false,
+                                })
+                            end
+                        else
+                            fallback()
+                        end
+                    end)
                 }),
                 sources = cmp.config.sources({
-                    -- { name = 'copilot' },
                     { name = 'nvim_lsp' },
                     { name = 'luasnip' },
+                    { name = 'copilot' },
                 }, {
                     { name = 'buffer' },
-                })
+                }),
+                window = {
+                    completion = cmp.config.window.bordered(),
+                    documentation = cmp.config.window.bordered(),
+                },
             })
 
             cmp.setup.filetype('gitcommit', {
                 sources = cmp.config.sources({
                     { name = 'git' },
+                    { name = 'path' }
                 }, {
                     { name = 'buffer' },
                 })
@@ -78,5 +122,12 @@ return {
             })
         end,
     },
-    {'L3MON4D3/LuaSnip', lazy = false },
+    {
+        'L3MON4D3/LuaSnip',
+        dependencies = { "rafamadriz/friendly-snippets" },
+        config = function ()
+            require("luasnip.loaders.from_vscode").lazy_load()
+        end,
+        lazy = false
+    },
 }
--- home-manager/configs/nvim/lua/plugins/copilot.lua
+++ home-manager/configs/nvim/lua/plugins/copilot.lua
@@ -1,14 +1,25 @@
 return {
-    -- {
-    --     "zbirenbaum/copilot.lua",
-    --     cmd = "Copilot",
-    --     event = "InsertEnter",
-    --     opts = {
-    --         suggestion = { enabled = false },
-    --         panel = { enabled = false },
-    --     },
-    --     dependencies = {
-    --         { "zbirenbaum/copilot-cmp", opts = {} },
-    --     }
-    -- },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    opts = {
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+    },
+    dependencies = {
+      { "zbirenbaum/copilot-cmp", opts = {} },
+    }
+  },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" },
+      { "nvim-lua/plenary.nvim" },
+    },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {},
+    -- See Commands section for default commands if you want to lazy load on them
+  },
 }
