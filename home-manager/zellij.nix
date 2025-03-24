c5d65c133a418a084ceb844add34c27068c687ff
Alexy Mantha <alexy@mantha.dev>
2025-03-23 22:28:23 -0400

    fix plugin path
    Signed-off-by: Alexy Mantha <alexy@mantha.dev>

--- home-manager/zellij.nix
+++ home-manager/zellij.nix
@@ -5,10 +5,13 @@
   };
 
   home.file.".config/zellij/config.kdl" = {
-        source = ./zellij.kdl;
+    source = ./zellij.kdl;
   };
 
   home.file.".config/zellij/layouts/default.kdl" = {
-        source = ./default.kdl;
+    text = builtins.replaceStrings 
+      ["ZJSTATUS_PATH"] 
+      ["${pkgs.zjstatus}"]
+      (builtins.readFile ./default.kdl);
   };
 }
