26215d2aa86e6fb363af48e7e73c3dfa188f24de
Alexy Mantha <alexy@mantha.dev>
2026-04-19 12:23:37 -0400

    correct hyprland binary
    Signed-off-by: Alexy Mantha <alexy@mantha.dev>

--- hosts/nixos/uwsm.nix
+++ hosts/nixos/uwsm.nix
@@ -1,10 +1,11 @@
-{...}: {
+{ ... }:
+{
   programs.uwsm.enable = true;
   programs.uwsm.waylandCompositors = {
     hyprland = {
       prettyName = "Hyprland";
       comment = "Hyprland compositor managed by UWSM";
-      binPath = "/run/current-system/sw/bin/Hyprland";
+      binPath = "/run/current-system/sw/bin/start-hyprland";
     };
   };
 }
